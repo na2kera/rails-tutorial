92331f182b2b401804a8fb7f5cb4e79976a598d5
na2kera <nat3mitaro@gmail.com>
2024-02-29 23:48:52 +0900

    バリデーションチェック

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+    # バリデーションチェック。入力された値が適切か判定
+    # タイトル入力必須
+    validates :title, presence: true
+    # body入力必須、十文字以上
+    validates :body, presence: true, length: { minimum: 10 }
 end

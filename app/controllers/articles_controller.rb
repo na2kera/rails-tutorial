d979a26659ceb095e407e8a5fb6c2839d2c185b2
na2kera <nat3mitaro@gmail.com>
2024-02-29 22:38:58 +0900

    showアクション

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,10 @@ class ArticlesController < ApplicationController
     # @で始まる変数はビューからでも参照できる
     @articles = Article.all
   end
+
+  # :idをroute.rbに書いたのでパラメータとして受け取る
+  # showアクションはデフォルトでapp/views/articles/show.html.erbをレンダリングします。
+  def show
+    @article = Article.find(params[:id])
+  end
 end

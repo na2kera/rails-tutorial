ba910cdc74f26b69a4a495be15ca2fcb3b532153
na2kera <nat3mitaro@gmail.com>
2024-02-29 22:57:30 +0900

    ルーティングをまとめた

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   root "articles#index"
 
   # ルーティングは、GET /articlesリクエストをArticlesControllerのindexアクションに対応付けます。
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "/articles", to: "articles#index"
+   # get "/articles/:id", to: "articles#show"
+  
+  # getで書いていたルーティングをresourcesで書き換え
+  resources  :articles
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+ 
 end

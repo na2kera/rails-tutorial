d941ed715ab1310a8440c0e427f93c467703a0fb
na2kera <nat3mitaro@gmail.com>
2024-03-02 16:29:54 +0900

    コメントの作成と保存のアクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+    def create
+        # 先に該当する記事をさがす
+        @article = Article.find(params[:article_id])
+        # createメソッドを使用することで作成と保存を同時に行う
+        @comment = @article.comments.create(comment_params)
+        # 元の記事に戻る
+        redirect_to article_path(@article)
+      end
+    
+      private
+        def comment_params
+          params.require(:comment).permit(:commenter, :body)
+        end
 end

55f655492aef3786e3763d56b42669c3b0f52623
na2kera <nat3mitaro@gmail.com>
2024-03-03 16:16:15 +0900

    認証をつける

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  # http_basic_authenticate_withメソッドでindexとshow以外は認証を要求する
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     # @で始まる変数はビューからでも参照できる
     @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    # コメントの削除を認証済みのユーザーだけもする
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
     def create
         # 先に該当する記事をさがす
         @article = Article.find(params[:article_id])

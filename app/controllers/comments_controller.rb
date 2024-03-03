0e4552811418cbcd940b3529abfe8b010b2dffe7
na2kera <nat3mitaro@gmail.com>
2024-03-03 12:43:42 +0900

    コメント削除のロジック

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,18 @@ class CommentsController < ApplicationController
         @comment = @article.comments.create(comment_params)
         # 元の記事に戻る
         redirect_to article_path(@article)
-      end
+    end
+
+    # コメント削除用のボタン
+    def destroy
+      # 対象の記事をさがす
+      @article = Article.find(params[:article_id])
+      # 対象のコメントをさがす
+      @comment = @article.comments.find(params[:id])
+      # 記事を削除
+      @comment.destroy
+      redirect_to article_path(@article), status: :see_other
+    end
     
       private
         def comment_params
